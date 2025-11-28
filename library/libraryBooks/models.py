e6bcab1fcd27efe7ae5c801e545db8651f09a2bb
brunor18 <brunoro343@gmail.com>
2025-11-28 12:18:22 -0300

    style: mundanca no front

--- library/libraryBooks/models.py
+++ library/libraryBooks/models.py
@@ -4,7 +4,7 @@ class Livros(models.Model):
     titulo = models.CharField(max_length=100, blank=False, null=False)
     autor = models.CharField(max_length=100, blank=False, null=False)
     editora = models.CharField(max_length=100, blank=False, null=False)
-    ISBN = models.IntegerField()
+    ISBN = models.IntegerField(max_length = 13, blank=False, null=False)
     genero = models.CharField(max_length=100, blank=False, null=False)
 
     objects = models.Manager()      
--- library/libraryBooks/urls.py
+++ library/libraryBooks/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from libraryBooks.views import index, cadastro, deletarCadastro,editarCadastro
+from libraryBooks.views import index, cadastro
 
 urlpatterns = [
     path('',index, name='index'),
     path('cadastro', cadastro, name='cadastro' ),
-    path('deletar/<int:id>/', deletarCadastro, name='deletarCadastro'),
-    path('editarCadastro/<int:id>/', editarCadastro, name='editarCadastro'),
 ]
