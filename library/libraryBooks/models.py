ea73dab0775f68827248bbee2a78772c5c773571
Bruno <brunoro343@gmail.com>
2025-12-05 15:06:54 -0300

    fix: agora o cadastro de livros funciona

--- library/libraryBooks/models.py
+++ library/libraryBooks/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+
 class Livros(models.Model):
     titulo = models.CharField(max_length=100, blank=False, null=False)
     autor = models.CharField(max_length=100, blank=False, null=False)
     editora = models.CharField(max_length=100, blank=False, null=False)
-    ISBN = models.IntegerField(max_length=13, blank=False, null=False)
+    ISBN = models.CharField(max_length=13, blank=False, null=False)
     genero = models.CharField(max_length=100, blank=False, null=False)
 
     objects = models.Manager()      
--- library/libraryBooks/views.py
+++ library/libraryBooks/views.py
@@ -5,62 +5,15 @@ from .forms import LivrosForm
 
 def index(request):
     lista = Livros.objects.all()
-    return render(request, 'index.html', {"Livross":lista})
+    return render(request, 'index.html', {"livros": lista})
 
 def cadastro(request):
     
     if request.method == 'POST':
         form = LivrosForm(request.POST)
-        if(form.is_valid()):
-            form.save()
-        else: 
-            HttpResponse(str(form.errors))
-            
-
-    return render(request, 'cadastro.html')
-    
-
-    # if request.method == 'POST': ##cadastrar Livros
-    #     titulo= request.POST.get('titulo')
-    #     ISBN= request.POST.get('ISBN')
-    #     autor= request.POST.get('autor')
-    #     editora= request.POST.get('editora')
-    #     genero= request.POST.get('genero')
-
-    #     Livros = Livros(
-    #                 titulo=titulo,
-    #                 ISBN=ISBN,
-    #                 autor=autor,
-    #                 editora=editora,
-    #                 genero=genero,
-    #                 )
-    #     Livros.save()
-
-        # return redirect('index')
-    
-
-def deletarCadastro(request, id):
-    Livros = Livros.objects.get(id=id)
-
-    if request.method == 'POST':
-        Livros.delete()
-        return redirect('index')  # volta para a lista de Livross
-
-    # GET request
-    return render(request, 'deletarCadastro.html', {'Livros': Livros})
-
-
-def editarCadastro(request, id):
-    Livros = Livros.objects.get(id=id)
-
-    if request.method == 'POST':
-        form = LivrosForm(request.POST, instance=Livros)
         if form.is_valid():
             form.save()
             return redirect('index')
-        else:
-            return HttpResponse(str(form.errors))
-
-    # GET request
-    form = LivrosForm(instance=Livros)
-    return render(request, 'editarCadastro.html', {'form': form, 'Livros': Livros})
+        else: 
+            HttpResponse(str(form.errors))
+    return render(request, 'cadastro.html')
